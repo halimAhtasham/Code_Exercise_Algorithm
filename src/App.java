90a65f3558d23289a2bf3d36e054b56500fe8f41
halimAhtasham <ahtashamhalim@gmail.com>
2025-07-03 22:39:24 +0600

    Add loop to print even and odd numbers in App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        for (int i = 0; i < 10; i++) {
+            while (i % 2 == 0 && i <= 10) {
+                System.out.println("Even number: " + i);
+                break;
+            }
+            if (i % 2 != 0) {
+                System.out.println("Odd number: " + i);
+            }
+        }
     }
 }
