2a4c841b8597b886e81cf3881a5208898771c00b
halimAhtasham <ahtashamhalim@gmail.com>
2025-07-04 22:28:06 +0600

    Add user input functionality and print entered value in App.java

--- src/App.java
+++ src/App.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         System.out.println("Hello, World!");
         for (int i = 0; i <= 10; i++) {
             if (i % 2 != 0) {
                 System.out.println("Odd number: " + i);
             }
         }
+        System.out.println("Enter a value: ");
+        int k = in.nextInt();
+        System.out.println("You entered the value: " + k);
+        in.close();
     }
 }
