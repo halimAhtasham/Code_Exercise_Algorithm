947b3e957f0b24d7d5b6bb49d5f1dbfe102b2600
halimAhtasham <ahtashamhalim@gmail.com>
2025-07-17 22:11:12 +0600

    Remove "Hello, World!" print statement from App.java

--- src/App.java
+++ src/App.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello, World!");
         for (int i = 0; i <= 10; i++) {
             if (i % 2 != 0) {
                 System.out.println("Odd number: " + i);
