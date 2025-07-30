9c0d98b3aba00bd60e75be8ddfbfa7e39f63e484
halimAhtasham <ahtashamhalim@gmail.com>
2025-07-31 00:17:35 +0600

    Add FactorialRecursion class to calculate factorials using recursion

---
+++ src/FactorialRecursion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class FactorialRecursion {
+    public static int counter = 0;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number to calculate its factorial:");
+        int n = in.nextInt();
+        long result = factorial(n);
+        System.out.println("Factorial of " + n + " is: " + result);
+        System.out.println("Total recursive calls: " + counter);
+        in.close();
+    }
+    private static long factorial(int n) {
+        counter++;
+        // TODO Auto-generated method stub
+        if (n <= 1) return 1l;
+        long result = factorial(n - 1);
+        return n * result;
+    }
+}
