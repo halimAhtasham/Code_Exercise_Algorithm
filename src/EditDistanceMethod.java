2d1b54583a6d597360fa5b81d437d338600cf2ee
halimAhtasham <ahtashamhalim@gmail.com>
2025-08-10 00:27:59 +0600

    Implement Levenshtein distance calculation and update EditDistanceMethod for user input

--- src/EditDistanceMethod.java
+++ src/EditDistanceMethod.java
@@ -1,39 +1,52 @@
 import java.util.Scanner;
 
-public class EditDistanceMethod {
-    static String A, B;
-    static int m, n, lcsLength;
-    static int[][]editDistanceMethod;
 
-    public static int lcs(String p, String q){
-        m = p.length();
-        n = q.length();
-        A = p;
-        B = q;
-        lcsLength = 0;
-        editDistanceMethod = new int[m +1][n+1];
+class LevenshteinDistance{
+    String S, T;
+    int m, n;
+    int[][] edit;
 
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                if(i == 0 || j == 0) editDistanceMethod[i][j] = 0;
-                else if(A.charAt(i-1) == B.charAt(j-1)){
-                    editDistanceMethod[i][j] = 1 + editDistanceMethod[i-1][j-1];
-                }
-                else{
-                    editDistanceMethod[i][j] = Math.max(editDistanceMethod[i-1][j], editDistanceMethod[i][j-1]);
-                }
-            }
-        }
-        return lcsLength = editDistanceMethod[m][n];
+    LevenshteinDistance(String p, String q){
+        S = p;
+        T = q;
+        m = S.length();
+        n = T.length();
+        edit = new int[m+1][n+1];
     }
+
+    public int computeDistance(String S, String T, int m, int n){
+        if(edit[m][n] != 0) return edit[m][n];
+        if(m == 0) return n;
+        if(n == 0) return m;
+        if(S.charAt(m-1) == T.charAt(n-1)){
+            return edit[m][n] = computeDistance(S, T, m-1, n-1); // no change
+        }
+        else{
+            return edit[m][n] = 1 + Math.min(
+                computeDistance(S, T, m-1, n-1),
+                Math.min(
+                    computeDistance(S, T, m-1, n),
+                    computeDistance(S, T, m, n-1)
+                )
+                );
+        }
+    }
+
+    public void printEditDistance(){
+        computeDistance(S, T, m, n);
+        System.out.println("Distance : " + edit[m][n]);
+    }
+}
+
+public class EditDistanceMethod {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String p, q;
         System.out.println("Enter two Strings: ");
         p = in.nextLine();
         q = in.nextLine();
-        int result = lcs(p,q);
-        System.out.println("Length of LCS: " + result);
+        LevenshteinDistance ld = new LevenshteinDistance(p , q);
+        ld.printEditDistance();
         in.close();
     }
 }
